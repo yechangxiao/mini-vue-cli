a2880517fec5dd4f1299f2180727e363bffeece6
yechangxiao <yechangxiaoycx@qq.com>
2022-05-08 00:52:07 +0800

    [perf]:开启js和css代码压缩，去除console

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -2,6 +2,8 @@
 const { merge } = require('webpack-merge')
 const base = require('./webpack.base')
 const webpack = require('webpack')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(base, {
   mode: 'production',
@@ -11,5 +13,17 @@ module.exports = merge(base, {
       'process.env.NODE_ENV': JSON.stringify('production'),
       'VueApp': JSON.stringify('prodApp') // 注入变量，在不同环境进行不同操作时很有用
     })
-  ]
+  ],
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true, // 去除console
+          }
+        }
+      })
+    ]
+  }
 })

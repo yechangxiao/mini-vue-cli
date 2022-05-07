51c8ea3e305474f3e68ef9eab1fd6f043af6a0e4
yechangxiao <yechangxiaoycx@qq.com>
2022-05-08 00:40:09 +0800

    [perf]:开启HMR

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -8,13 +8,17 @@ module.exports = merge(base, {
   mode: 'development',
   devServer: {
     port: 9000,
-    open: true
+    open: true,
+    hot: 'only'
   },
   devtool: 'eval-cheap-module-source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
       'VueApp': JSON.stringify('devApp')
-    })
+    }),
+    // 以前在webpack或webpack-dev-server中配置--hot，就不需要配置下面的插件
+    // 现在的高版本webpack都不需要了
+    // new webpack.HotModuleReplacementPlugin()
   ]
 })
